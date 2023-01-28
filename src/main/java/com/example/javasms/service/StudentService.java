c93c386dd86087e8d093ececf6454251458f594a
Arvind Kumar <arvindkumar2575@gmail.com>
2023-01-29 01:20:49 +0530

    initial commit

---
+++ src/main/java/com/example/javasms/service/StudentService.java
@@ -0,0 +1,17 @@
+package com.example.javasms.service;
+
+import java.util.List;
+
+import com.example.javasms.entility.Student;
+
+public interface StudentService {
+	List<Student> getAllStudents();
+	
+	Student saveStudent(Student student);
+	
+	Student getStudentById(Long id);
+	
+	Student updateStudent(Student student);
+	
+	void deleteStudentById(Long id);
+}
